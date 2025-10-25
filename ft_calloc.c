a2116a8f93fa108632d9b34562c2230fbf9b1e4a
Alexandre <87145504+0verLighT@users.noreply.github.com>
2025-10-25 12:36:35 +0200

    fix: ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -12,17 +12,16 @@
 
 #include "libft.h"
 
+#include "libft.h"
+
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t	len;
 	void	*ptr;
-	
-	len = nmemb * size;
-	if (len == 0 || len / nmemb != size)
-	{
-		ptr = malloc(0);
-		return (ptr);
-	}
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	if (nmemb > (size_t)- 1 / size)
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
