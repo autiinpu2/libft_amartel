ee7012165ae90d5a930b08906d30b41ca93d47e6
0verLighT <alexandre@0verlight.com>
2026-01-21 02:03:24 +0100

    fix: shell.nix

--- shell.nix
+++ shell.nix
@@ -1,21 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell rec {
-  stdenv = pkgs.clangStdenv;
-
+(pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }) {
   packages = with pkgs; [
     doxygen
     graphviz
     meson
     ninja
   ]
-  ++ pkgs.lib.optionals stdenv.isLinux [
+  ++ lib.optionals stdenv.isLinux [
     valgrind
     gdb
   ];
-
-  shellHook = ''
-    echo "direnv for lift"
-    cc --version
-  '';
+  shellHook = ''cc --version'';
 }
