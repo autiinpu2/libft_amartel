e8312bb1a241a3ddddc2c102fa1462a19e48718d
0verLighT <alexandre@0verlight.com>
2026-01-24 21:52:34 +0100

    docs: update is.h
    Signed-off-by: 0verLighT <alexandre@0verlight.com>

--- includes/is.h
+++ includes/is.h
@@ -6,7 +6,7 @@
 /*   By: amartel <amartel@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 04:06:43 by amartel           #+#    #+#             */
-/*   Updated: 2026/01/21 15:01:21 by amartel          ###   ########.fr       */
+/*   Updated: 2026/01/24 21:51:58 by amartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@
 /**
  * @brief Checks for an alphabetic character.
  * @param c The character to test.
- * @return Non-zero if c is an alphabetic character, 0 otherwise.
+ * @return true if c is an alphabetic character, false otherwise.
  */
 bool	ft_isalpha(const int c);
 
 /**
  * @brief Checks for a digit (0 through 9).
  * @param c The character to test.
- * @return Non-zero if c is a digit, 0 otherwise.
+ * @return true if c is a digit, false otherwise.
  */
 bool	ft_isdigit(const int c);
 
 /**
  * @brief Checks for an alphanumeric character.
  * @param c The character to test.
- * @return Non-zero if c is alphanumeric, 0 otherwise.
+ * @return true if c is alphanumeric, false otherwise.
  */
 bool	ft_isalnum(const int c);
 
@@ -43,21 +43,21 @@ bool	ft_isalnum(const int c);
  * @brief Checks whether c is a 7-bit unsigned char value that fits into the 
  * ASCII character set.
  * @param c The character to test.
- * @return Non-zero if c is an ASCII character, 0 otherwise.
+ * @return true if c is an ASCII character, false otherwise.
  */
 bool	ft_isascii(const int c);
 
 /**
  * @brief Checks for any printable character (including space).
  * @param c The character to test.
- * @return Non-zero if c is printable, 0 otherwise.
+ * @return true if c is printable, false otherwise.
  */
 bool	ft_isprint(const int c);
 
 /**
- * @brief
- * @param
- * @return
+ * @brief Checks for an speace character
+ * @param c thes character to test.
+ * @return true if c is space, false otherwise.
  */
 bool	ft_isspace(const int c);
 
