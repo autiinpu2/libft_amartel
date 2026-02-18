405be004c600761fa6872d8590741284fe768524
0verLighT <alexandre@0verlight.com>
2026-02-18 05:16:15 +0100

    fix: change ft_putnbr recursive to iterative function
    Signed-off-by: 0verLighT <alexandre@0verlight.com>

--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -6,33 +6,49 @@
 /*   By: amartel <amartel@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 23:47:22 by amartel           #+#    #+#             */
-/*   Updated: 2026/02/01 01:43:24 by amartel          ###   ########.fr       */
+/*   Updated: 2026/02/18 05:15:21 by amartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	precheck(int nb, int fd, int *len)
+{
+	if (fd < 0)
+		return (1);
+	if (nb == 0)
+	{
+		ft_putchar_fd('0', fd);
+		++*len;
+		return (1);
+	}
+	return (0);
+}
+
 int	ft_putnbr_fd(int n, int fd)
 {
-	int	i;
+	long	divider;
+	long	nb;
+	int		i;
 
+	nb = n;
+	divider = 1;
 	i = 0;
-	if (fd >= 0)
+	if (precheck(nb, fd, &i) == 1)
+		return (i);
+	if (nb < 0)
 	{
-		if (n == -2147483648)
-			i += ft_putstr_fd("-2147483648", fd);
-		else if (n < 0)
-		{
-			i += ft_putchar_fd('-', fd);
-			i += ft_putnbr_fd(-n, fd);
-		}
-		else if (n > 9)
-		{
-			i += ft_putnbr_fd(n / 10, fd);
-			i += ft_putnbr_fd(n % 10, fd);
-		}
-		else
-			i += ft_putchar_fd(n + '0', fd);
+		ft_putchar_fd('-', fd);
+		nb = -nb;
+	}
+	while (divider * 10 <= nb)
+		divider *= 10;
+	while (divider > 0)
+	{
+		ft_putchar_fd((nb / divider + '0'), fd);
+		nb %= divider;
+		divider /= 10;
+		++i;
 	}
 	return (i);
 }
